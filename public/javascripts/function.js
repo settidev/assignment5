6b083f1961a598d45466194ebb15550e5d8e1a46
settidev <satveerbhogal@gmail.com>
2021-08-04 17:23:57 +0530

    First commit

---
+++ public/javascripts/function.js
@@ -0,0 +1,13 @@
+function home(){
+    document.title = "MY Home";
+    console.log("satveer home");
+}
+function project(){
+    document.title = "Project";
+}
+function contact(){
+    document.title = "Contact";
+}
+function resume(){
+    document.title = "Resume";
+}
